fefa58e9950cdf412ad613457d1f352233cc13be
AdDper <1570919831@qq.com>
2017-09-08 09:41:10 +0800

    代码提交

---
+++ WebViewDemo/app/src/main/java/com/zhuoxun/webviewdemo/MainActivity.java
@@ -0,0 +1,247 @@
+package com.zhuoxun.webviewdemo;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tencent.smtt.export.external.interfaces.IX5WebChromeClient;
+import com.tencent.smtt.sdk.CookieSyncManager;
+import com.tencent.smtt.sdk.DownloadListener;
+import com.tencent.smtt.sdk.QbSdk;
+import com.tencent.smtt.utils.TbsLog;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private com.tencent.smtt.sdk.WebView webView;
+
+    private static final String mHomeUrl = "https://www.sogou.com";
+    private static final String TAG = "SdkDemo";
+    private static final int MAX_LENGTH = 14;
+    private boolean mNeedTestPage = false;
+
+
+    public static final int MSG_OPEN_TEST_URL = 0;
+    public static final int MSG_INIT_UI = 1;
+    private final int mUrlStartNum = 0;
+    private int mCurrentUrl = mUrlStartNum;
+
+    private URL mIntentUrl;
+
+    private Handler mTestHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_OPEN_TEST_URL:
+                    if (!mNeedTestPage) {
+                        return;
+                    }
+
+                    String testUrl = "file:///sdcard/outputHtml/html/"
+                            + Integer.toString(mCurrentUrl) + ".html";
+                    if (mWebView != null) {
+                        mWebView.loadUrl(testUrl);
+                    }
+
+                    mCurrentUrl++;
+                    break;
+                case MSG_INIT_UI:
+                    init();
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
+    private X5WebView mWebView;
+    private ViewGroup mViewParent;
+
+    private void init() {
+
+        mWebView = new X5WebView(this, null);
+
+        mViewParent.addView(mWebView, new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.FILL_PARENT,
+                FrameLayout.LayoutParams.FILL_PARENT));
+
+        mWebView.setWebViewClient(new com.tencent.smtt.sdk.WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(com.tencent.smtt.sdk.WebView view, String url) {
+                return false;
+            }
+
+            @Override
+            public void onPageFinished(com.tencent.smtt.sdk.WebView view, String url) {
+                super.onPageFinished(view, url);
+                // mTestHandler.sendEmptyMessage(MSG_OPEN_TEST_URL);
+                mTestHandler.sendEmptyMessageDelayed(MSG_OPEN_TEST_URL, 5000);// 5s?
+//                if (Integer.parseInt(android.os.Build.VERSION.SDK) >= 16)
+//                    changGoForwardButton(view);
+				/* mWebView.showLog("test Log"); */
+            }
+        });
+
+        mWebView.setWebChromeClient(new com.tencent.smtt.sdk.WebChromeClient() {
+
+            @Override
+            public boolean onJsConfirm(com.tencent.smtt.sdk.WebView arg0, String arg1, String arg2,
+                                       com.tencent.smtt.export.external.interfaces.JsResult arg3) {
+                return super.onJsConfirm(arg0, arg1, arg2, arg3);
+            }
+
+            View myVideoView;
+            View myNormalView;
+            IX5WebChromeClient.CustomViewCallback callback;
+
+            // /////////////////////////////////////////////////////////
+            //
+            /**
+             * 全屏播放配置
+             */
+/*            @Override
+            public void onShowCustomView(View view,
+                                         IX5WebChromeClient.CustomViewCallback customViewCallback) {
+                FrameLayout normalView = (FrameLayout) findViewById(R.id.web_filechooser);
+                ViewGroup viewGroup = (ViewGroup) normalView.getParent();
+                viewGroup.removeView(normalView);
+                viewGroup.addView(view);
+                myVideoView = view;
+                myNormalView = normalView;
+                callback = customViewCallback;
+            }*/
+
+            @Override
+            public void onHideCustomView() {
+                if (callback != null) {
+                    callback.onCustomViewHidden();
+                    callback = null;
+                }
+                if (myVideoView != null) {
+                    ViewGroup viewGroup = (ViewGroup) myVideoView.getParent();
+                    viewGroup.removeView(myVideoView);
+                    viewGroup.addView(myNormalView);
+                }
+            }
+
+            @Override
+            public boolean onJsAlert(com.tencent.smtt.sdk.WebView arg0, String arg1, String arg2,
+                                     com.tencent.smtt.export.external.interfaces.JsResult arg3) {
+                /**
+                 * 这里写入你自定义的window alert
+                 */
+                return super.onJsAlert(null, arg1, arg2, arg3);
+            }
+        });
+
+
+        com.tencent.smtt.sdk.WebSettings webSetting = mWebView.getSettings();
+        webSetting.setAllowFileAccess(true);
+        webSetting.setLayoutAlgorithm(com.tencent.smtt.sdk.WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSetting.setSupportZoom(true);
+        webSetting.setBuiltInZoomControls(true);
+        webSetting.setUseWideViewPort(true);
+        webSetting.setSupportMultipleWindows(false);
+        // webSetting.setLoadWithOverviewMode(true);
+        webSetting.setAppCacheEnabled(true);
+        // webSetting.setDatabaseEnabled(true);
+        webSetting.setDomStorageEnabled(true);
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setGeolocationEnabled(true);
+        webSetting.setAppCacheMaxSize(Long.MAX_VALUE);
+        webSetting.setAppCachePath(this.getDir("appcache", 0).getPath());
+        webSetting.setDatabasePath(this.getDir("databases", 0).getPath());
+        webSetting.setGeolocationDatabasePath(this.getDir("geolocation", 0)
+                .getPath());
+        // webSetting.setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);
+        webSetting.setPluginState(com.tencent.smtt.sdk.WebSettings.PluginState.ON_DEMAND);
+        // webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        // webSetting.setPreFectch(true);
+        long time = System.currentTimeMillis();
+        if (mIntentUrl == null) {
+            mWebView.loadUrl(mHomeUrl);
+        } else {
+            mWebView.loadUrl(mIntentUrl.toString());
+        }
+        TbsLog.d("time-cost", "cost time: "
+                + (System.currentTimeMillis() - time));
+        CookieSyncManager.createInstance(this);
+        CookieSyncManager.getInstance().sync();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        getWindow().setFormat(PixelFormat.TRANSLUCENT);//（这个对宿主没什么影响，建议声明）
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            try {
+                mIntentUrl = new URL(intent.getData().toString());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (NullPointerException e) {
+
+            } catch (Exception e) {
+            }
+        }
+        //
+        try {
+            if (Integer.parseInt(android.os.Build.VERSION.SDK) >= 11) {
+                getWindow()
+                        .setFlags(
+                                android.view.WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                                android.view.WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+            }
+        } catch (Exception e) {
+        }
+
+		/*
+		 * getWindow().addFlags(
+		 * android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		 */
+        setContentView(R.layout.activity_main);
+
+        mViewParent = (ViewGroup) findViewById(R.id.frame);
+
+
+        mTestHandler.sendEmptyMessageDelayed(MSG_INIT_UI, 10);
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (webView != null && webView.canGoBack()) {
+                webView.goBack();
+                return true;
+            } else
+                return super.onKeyDown(keyCode, event);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
